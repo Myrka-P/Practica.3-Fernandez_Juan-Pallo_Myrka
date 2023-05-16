ac1c6340857e6996c1246c3c0ed345f4925fd750
Myrka <Myrka@172.16.219.197>
2023-05-16 11:04:38 -0500

    UNIDAD 2 EJEMPLO OCHO

---
+++ Prac_3_fernandez___Pallo/src/main/java/ec/edu/ups/prac_3_fernandez___pallo/Vista/Prac_3_fernandez___Pallo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.prac_3_fernandez___pallo.Vista;
+
+/**
+ *
+ * @author Myrka
+ */
+public class Prac_3_fernandez___Pallo {
+
+    public static void main(String[] args) {
+        playa
+    }
+}
